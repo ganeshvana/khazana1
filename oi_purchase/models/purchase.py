63c32855e0279df6c40f5a4ea59f4d8b63520324
Kiruthikapoorani A <krithipoorani92@gmail.com>
2022-05-03 07:01:00 +0530

    modular

--- oi_purchase/models/purchase.py
+++ oi_purchase/models/purchase.py
@@ -51,10 +51,10 @@ class Purchase(models.Model):
         if self.partner_id.name == 'Modular':
             info = xmlrpc.client.ServerProxy('https://ganeshvana-khazana-april-modular-4577685.dev.odoo.com/xmlrpc/2/common')
             url = self.env["ir.config_parameter"].sudo().get_param("module.url")
-            # url = 'https://ganeshvana-khazana-april-modular-4577685.dev.odoo.com'
+            url = 'https://ganeshvana-khazana1-modular-4805725.dev.odoo.com'
             common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-            url = self.env["ir.config_parameter"].sudo().get_param("module.db")
-            # db = 'ganeshvana-khazana-april-modular-4577685'
+            db = self.env["ir.config_parameter"].sudo().get_param("module.db")
+            db = 'ganeshvana-khazana-april-modular-4577685'
             uid = self.env["ir.config_parameter"].sudo().get_param("module.uid")
             # uid = 2
             password = self.env["ir.config_parameter"].sudo().get_param("module.password")
