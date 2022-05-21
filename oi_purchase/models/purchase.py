1e32b75f0699f2dcc5ae59cf28f29f7cf6ac08c5
Kiruthikapoorani A <krithipoorani92@gmail.com>
2022-05-21 07:14:05 +0530

    sale warning

--- oi_purchase/models/purchase.py
+++ oi_purchase/models/purchase.py
@@ -61,7 +61,11 @@ class Purchase(models.Model):
             password = 'admin'
             modells = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
             customer = modells.execute_kw(db, uid, password, 'res.partner', 'search', [[['name', '=', 'Khazana']]])
-            sale = modells.execute_kw(db, uid, password, 'sale.order', 'create', [{'partner_id': customer[0],'client_order_ref': self.name + self.origin}])
+            if self.origin:
+                org = self.origin
+            else:
+                org = ''
+            sale = modells.execute_kw(db, uid, password, 'sale.order', 'create', [{'partner_id': customer[0],'client_order_ref': self.name + org}])
             sale_ref = modells.execute_kw(db, uid, password, 'sale.order', 'search', [[['id', '=', sale]]])
             self.partner_ref = sale_ref
             for line in self.order_line:
